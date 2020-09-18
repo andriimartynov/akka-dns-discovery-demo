e1b0d0dab2ff4e800b460fbb4898385be769bc75
andriimartynov <andrii.martynov@gmail.com>
2020-09-18 09:25:55 +0300

    dependencies updated

--- project/Akka.scala
+++ project/Akka.scala
@@ -1,11 +1,12 @@
 import sbt._
 
 object Akka {
-  lazy val AKKA_VERSION: String = "2.5.27"
-  lazy val AKKA_HTTP_VERSION: String = "10.1.11"
-  lazy val AKKA_MANAGEMENT_VERSION: String = "1.0.6"
+  lazy val AKKA_VERSION: String = "2.5.31"
+  lazy val AKKA_HTTP_VERSION: String = "10.1.12"
+  lazy val AKKA_MANAGEMENT_VERSION: String = "1.0.8"
 
   lazy val dependencies: Seq[ModuleID] = Seq(
+    "com.typesafe.akka" %% "akka-cluster" % AKKA_VERSION,
     "com.typesafe.akka" %% "akka-discovery" % AKKA_VERSION,
     "com.typesafe.akka" %% "akka-remote" % AKKA_VERSION,
     "com.typesafe.akka" %% "akka-slf4j" % AKKA_VERSION
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,6 +2,6 @@ logLevel := Level.Warn
 
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.7.4")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.7.5")
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.7.0")
